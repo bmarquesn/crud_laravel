7bdcf95c8aae7c2aae8456cc950f251c33d5f06e
BRUNONOMAR\Bruno <bmarquesn@gmail.com>
2020-07-16 09:41:47 -0300

    Inicializacao do CRUD Laravel

---
+++ resources/views/produtos/create.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>CRUD laravel - Criar Produto</title>
+  </head>
+  <body>
+    <form action="">
+		<div class="row">
+			<div class="col"><label for="nome">Nome:</label></div>
+			<div class="col"><input type="text" name="nome" id="nome" value="" /></div>
+		</div>
+		<div class="row">
+			<div class="col"><label for="nome">Nome:</label></div>
+			<div class="col"><input type="text" name="nome" id="nome" value="" /></div>
+		</div>
+		<div class="row">
+			<div class="col"><label for="nome">Nome:</label></div>
+			<div class="col"><input type="text" name="nome" id="nome" value="" /></div>
+		</div>
+		<div class="row">
+			<div class="col"><label for="nome">Nome:</label></div>
+			<div class="col"><input type="text" name="nome" id="nome" value="" /></div>
+		</div>
+	</form>
+  </body>
+</html>
