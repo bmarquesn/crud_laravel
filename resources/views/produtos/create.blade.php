0777b3221ef45b3e38a4bf48a3ad3928969d6d52
BRUNONOMAR\Bruno <bmarquesn@gmail.com>
2020-07-16 11:12:57 -0300

    Primeira parte do CRUD Produtos (insercao)

--- resources/views/produtos/create.blade.php
+++ resources/views/produtos/create.blade.php
@@ -7,22 +7,26 @@
     <title>CRUD laravel - Criar Produto</title>
   </head>
   <body>
-    <form action="">
+    <form action="{{ route('registrar_produto') }}" method="post">
+		@csrf
 		<div class="row">
 			<div class="col"><label for="nome">Nome:</label></div>
 			<div class="col"><input type="text" name="nome" id="nome" value="" /></div>
 		</div>
 		<div class="row">
-			<div class="col"><label for="nome">Nome:</label></div>
-			<div class="col"><input type="text" name="nome" id="nome" value="" /></div>
+			<div class="col"><label for="custo">Custo:</label></div>
+			<div class="col"><input type="text" name="custo" id="custo" value="" /></div>
 		</div>
 		<div class="row">
-			<div class="col"><label for="nome">Nome:</label></div>
-			<div class="col"><input type="text" name="nome" id="nome" value="" /></div>
+			<div class="col"><label for="preco">Preço:</label></div>
+			<div class="col"><input type="text" name="preco" id="preco" value="" /></div>
 		</div>
 		<div class="row">
-			<div class="col"><label for="nome">Nome:</label></div>
-			<div class="col"><input type="text" name="nome" id="nome" value="" /></div>
+			<div class="col"><label for="quantidade">Quantidade:</label></div>
+			<div class="col"><input type="text" name="quantidade" id="quantidade" value="" /></div>
+		</div>
+		<div class="row">
+			<div class="col"><input type="submit" value="Salvar" /></div>
 		</div>
 	</form>
   </body>
